58ef0bb0b49c3a6842300d1d9ac239f6d675f465
Echzio <aleksassincreed@yandex.ru>
2019-03-28 23:25:59 +0300

    [WIP] add favico, fix particles.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,90 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const postcssPresetEnv = require('postcss-preset-env');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'awesome.js',
-        publicPath: '',
-    },
-    devServer: {
-        overlay: true,
-        hot: true,
-        port: 9000
-    },
-    module: {
-        rules: [
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'awesome.js',
+    publicPath: '',
+  },
+  devServer: {
+    overlay: true,
+    hot: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
 
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }]
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             },
-
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [{
-                        loader: 'style-loader'
-                    }, {
-                        loader: MiniCssExtractPlugin.loader
-                    }, {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                postcssPresetEnv({
-                                    state : 0,
-                                    browsers: ['>1%'],
-                                    autoprefixer: { grid: true }
-                                })
-                            ],
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                postcssPresetEnv({
+                  state: 0,
+                  browsers: ['>1%'],
+                  autoprefixer: { grid: true },
+                }),
+              ],
+              sourceMap: true,
             },
-                {
-                    test: /\.(png|jpg)$/,
-                    loader: 'url-loader'
-                }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    optimization: {
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  optimization: {
     minimizer: [new TerserPlugin()],
   },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),       
-    ]
-}
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/img/favicon.ico',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
